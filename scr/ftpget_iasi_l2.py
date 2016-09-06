6a99ec213fcb9dae2d8cf7040938729dd614c3c9
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2016-09-06 14:17:06 +0200

    Specify the time window to consider as an argument when doing ftp retrieval
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- scr/ftpget_iasi_l2.py
+++ scr/ftpget_iasi_l2.py
@@ -55,7 +55,6 @@ HOST = OPTIONS['ftp_host']
 REMOTE_DIRS = [OPTIONS['remote_dir'], ]
 USER = OPTIONS['login_user']
 PASSWD = OPTIONS['login_passwd']
-DAYS_IN_PAST = int(OPTIONS['time_window_days'])
 
 #: Default time format
 _DEFAULT_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
@@ -78,69 +77,69 @@ if __name__ == "__main__":
         description='ftp retrieval of EARS IASI lvl2 data')
     parser.add_argument("-d", "--dir",
                         help="Destination directory path")
+    parser.add_argument("--hours",
+                        help="How far back in time in hours to fetch",
+                        type=int)
 
     args = parser.parse_args()
     outpath = args.dir
+    hours = args.hours
 
     today = datetime.today()
-    ndays = timedelta(DAYS_IN_PAST)
-    yesterday = today - ndays
+    dtime = timedelta(seconds=3600 * hours)
+    starttime = today - dtime
 
     # IASI_PW3_02_M01_20160309180258Z_20160309180554Z_N_O_20160309184345Z.h5
     pattern = 'IASI_PW3_02_{platform_name:3s}_{start_time:%Y%m%d%H%M%S}Z_{end_time:%Y%m%d%H%M%S}Z_N_O_{creation_time:%Y%m%d%H%M%S}Z.h5'
     p__ = Parser(pattern)
 
-    for day in [today, yesterday]:
-        dayofyear = day.strftime("%j")
-        year = day.strftime("%Y")
+    PREFIXES = ['IASI_PW3_', ]
+    ftp = FTP(HOST)
+    print ("connecting to %s" % HOST)
+    ftp.login(USER, PASSWD)
+    tempfile.tempdir = outpath
+    for (remotedir, prefix) in zip(REMOTE_DIRS, PREFIXES):
+        remotefiles = ftp.nlst(remotedir)
+        fnames = [os.path.basename(f) for f in remotefiles]
+        dates_remote = [p__.parse(s)['start_time'] for s in fnames]
 
-        PREFIXES = ['IASI_PW3_', ]
-        ftp = FTP(HOST)
-        print ("connecting to %s" % HOST)
-        ftp.login(USER, PASSWD)
-        tempfile.tempdir = outpath
-        for (remotedir, prefix) in zip(REMOTE_DIRS, PREFIXES):
-            remotefiles = ftp.nlst(remotedir)
-            fnames = [os.path.basename(f) for f in remotefiles]
-            dates_remote = [p__.parse(s)['start_time'] for s in fnames]
+        rfarr = np.array(remotefiles)
+        drarr = np.array(dates_remote)
+        remotefiles = rfarr[drarr > starttime].tolist()
+        remotefiles = [r for r in remotefiles if (
+            r.endswith('.h5') and os.path.basename(r).startswith(prefix))]
 
-            rfarr = np.array(remotefiles)
-            drarr = np.array(dates_remote)
-            remotefiles = rfarr[drarr > day - ndays].tolist()
-            remotefiles = [r for r in remotefiles if (
-                r.endswith('.h5') and os.path.basename(r).startswith(prefix))]
+        localfiles = [
+            os.path.join(outpath,
+                         os.path.basename(f.strip('.h5'))) for f in remotefiles]
 
-            localfiles = [
-                os.path.join(outpath,
-                             os.path.basename(f.strip('.h5'))) for f in remotefiles]
+        try:
+            for (remote_file, local_file) in zip(remotefiles, localfiles):
+                if not os.path.isfile(local_file + '.h5'):
 
-            try:
-                for (remote_file, local_file) in zip(remotefiles, localfiles):
-                    if not os.path.isfile(local_file + '.nc'):
+                    out_filename = local_file + '.h5'
+                    tmpfilename = tempfile.mktemp()
+                    try:
+                        file_handle = open(tmpfilename, 'wb')
+                        ftp.retrbinary(
+                            'RETR ' + remote_file, file_handle.write)
+                        file_handle.close()
+                    except socket.error:
+                        print("Cannot get file %s" % remote_file)
+                        if os.path.exists:
+                            os.remove(tmpfilename)
+                        continue
 
-                        out_filename = local_file + '.h5'
-                        tmpfilename = tempfile.mktemp()
-                        try:
-                            file_handle = open(tmpfilename, 'wb')
-                            ftp.retrbinary(
-                                'RETR ' + remote_file, file_handle.write)
-                            file_handle.close()
-                        except socket.error:
-                            print("Cannot get file %s" % remote_file)
-                            if os.path.exists:
-                                os.remove(tmpfilename)
-                            continue
+                    os.rename(tmpfilename, out_filename)
+                    print("Retrieved file %s" % (out_filename))
+                else:
+                    print("File already saved, skipping: %s.h5" %
+                          (local_file))
 
-                        os.rename(tmpfilename, out_filename)
-                        print("Retrieved file %s" % (out_filename))
-                    else:
-                        print("File already saved, skipping: %s.h5" %
-                              (local_file))
+        except socket.error:
+            print "Exiting"
+            ftp.quit()
+            break
 
-            except socket.error:
-                print "Exiting"
-                ftp.quit()
-                break
-
-        print "Exiting"
-        ftp.quit()
+    print "Exiting"
+    ftp.quit()
